955d8f8c3bbd142ada0d7c80940b940b22f6c495
Fábio Rocha de Lima <spartakfabi@gmail.com>
2022-12-23 19:04:51 -0300

    Pasta em java responsável pelos arquivos em java que esta integrado com as páginas de html

---
+++ java_aula4/alerta.js
@@ -0,0 +1,5 @@
+window.alert('Cuidado!');//disparar alerta
+window.confirm('Está gostando da aula?');//janela com botão ok e cancelar 
+var nome=prompt('Qual é o seu nome?');//vai perguntar o nome
+window.alert(`Seja bem vindo ${nome} ao sistema.`);
+document.writeln(`Olá<strong> ${nome}</strong> seja bem vindo ao sistema!`)
